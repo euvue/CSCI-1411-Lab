d1948b1041dfb5ee83a5df88050ed4564b0c774a
euvue <eeuv@tuta.io>
2020-12-16 22:25:35 -0700

    init commit

---
+++ Lab03./Lab03.py
@@ -0,0 +1,11 @@
+# Name: Eupheng Vue
+# Class: CSCI 1411
+# File: EuphengVueLab03.py
+# This lab is a program that asks the user input of length
+# and width of a box in inches.
+# It calculates the perimeter of the box, how many trims
+# boards the user will need,
+# the cost of the total trim boards, 
+# how much trim is wasted, and
+# how much money the user wasted.
+
