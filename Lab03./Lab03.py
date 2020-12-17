9e9309e17e38025e659c5b8fe56c6b747bc70f9a
euvue <eeuv@tuta.io>
2020-12-16 22:35:06 -0700

    define all variable needed

--- Lab03./Lab03.py
+++ Lab03./Lab03.py
@@ -17,3 +17,17 @@ def main():
     length_box = int ( input ('Enter the length of the box in inches: ' ))   # prompts user input for the length of the box
 
     width_box = float ( input ('Enter the width of the box in inches: ' ))   # prompts user input for the width of the box
+
+    perimeter_box = ( 2 * length_box ) + ( 2 * width_box)    # perimeter formula of the input length & width of the box
+
+    segment_trim = 12      # total of inches per segement trim
+
+    price_trime = 12       # price per segment trim
+
+    total_trim = round ( perimeter_box / segment_trim)       # formula for total trim the user needs for the box
+
+    total_price_trim = ( total_trim * price_trim )            # total price of the total trim the user needs
+
+    loss_trim = round( ( ( segment_trim * total_trim ) - perimeter_box ), 3 )       # total trim wasted forumla
+
+    loss_money = round( ( ( loss_trim/segment_trim ) * price_trim ), 2 )          # total loss of money from the wasted trim
