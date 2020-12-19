c2e6944785f21e6362da91aae11cf4f544d7c1e0
euvue <eeuv@tuta.io>
2020-12-18 17:57:58 -0700

    add Lab05

---
+++ Lab05/Lab05.py
@@ -0,0 +1,24 @@
+# File: vueEuphengLab05a.py
+# Eupheng
+# Vue
+# Date: September 17,2020
+# Description: Lab 5. This lab shows various operations of String datatype
+
+# You can calculate the size/length of the string using len() function
+
+    length = len(firstName)
+    print("The length of your first name is: ", length)
+
+    length_2 = len(lastName)
+    print("The length of your last name is: ", length_2)
+
+    # "+" operator is used to concatenate 2 strings
+
+    fullName = firstName + " " + lastName
+    print("Your full name is: ", fullName)
+
+    # "*" operator is used as perpetition operator to make multiple copies of a string.
+
+    n = int(input("Enter a number: "))
+    print("Your full name is: ", fullName)
+    print((firstName + " ")* n)
